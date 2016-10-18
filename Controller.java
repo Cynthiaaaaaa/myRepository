59dff4bfb99418d85333e56a808438d107553e50
zhangyufeng <zhangyufeng@didichuxing.com>
2016-10-18 22:10:49 +0800

    add only one

--- Controller.java
+++ Controller.java
@@ -2,6 +2,7 @@ public class Controller{
     public static void main(String [] args){
         int num = 0;
         int result = num+10;
+        int mul = result*2;
         System.out.println(result);
     }
 }
