9a87bfbb566340e1a3a0d9f5f2d8430a9350cbe6
zhangyufeng <zhangyufeng@didichuxing.com>
2016-10-18 22:52:29 +0800

    add adminvo.java

---
+++ src/AdminVO.java
@@ -0,0 +1,9 @@
+public class AdminVO{
+    private String adminname;
+    public void setAdminname(String adminname){
+        this.adminname = adminname;
+    }
+    public String getAdminname(){
+        return this.adminname;
+    }
+}
