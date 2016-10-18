446f54fc70cb4d6a53bf3081f2ff2c0f2cf93267
zhangyufeng <zhangyufeng@didichuxing.com>
2016-10-18 19:33:25 +0800

    add userVO.java

---
+++ src/UserVO.java
@@ -0,0 +1,9 @@
+public class UserVO{
+    private String username;
+    public void setUserName(String username){
+        this.username = username;
+    }
+    public String getUserName(){
+        return this.username;
+    }
+}
