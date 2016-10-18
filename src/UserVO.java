46373128a4f5453b9c770a949aed9f3badc51643
zhangyufeng <zhangyufeng@didichuxing.com>
2016-10-18 22:51:59 +0800

    delete

--- src/UserVO.java
+++
@@ -1,9 +0,0 @@
-public class UserVO{
-    private String username;
-    public void setUsernama(String username){
-        this.username = username;
-    }
-    public String getUsername(){
-    return this.username;
-    }
-}
