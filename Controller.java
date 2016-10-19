2d150fe6a6876237854106fd813e2c29ffc5ab83
zhangyufeng <zhangyufeng@didichuxing.com>
2016-10-19 16:55:18 +0800

    modify controller

--- Controller.java
+++ Controller.java
@@ -1,9 +1,5 @@
 public class Controller{
     public static void main(String [] args){
-        int num = 0;
-        int result = num+10;
-        int mul = result*2;
-        int mul2 = mul*2;
         String time2230 = "2230";
         String time1311 = "1311";
         String time1315 = "1315";
@@ -11,6 +7,6 @@ public class Controller{
         String time1317 = "1317";
         String time1512 = "1512";
         int time427 = 427;
-        System.out.println(result);
+        int time1700 = 1700;
     }
 }
